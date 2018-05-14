ae6fbbf94f569e5a6c528198145d8cdb1790ea08
wangli <li_wangli7@126.com>
2018-05-14 13:19:56 +0800

    this is a test

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -1,49 +1,58 @@
 #!/usr/bin/python
-#-*- coding:utf-8 -*-
-'''
+# -*- coding:utf-8 -*-
+"""
 数据库连接模块
-'''
+"""
+
 import threading
 import functools
-#数据库引擎对象
+# 数据库引擎对象
 engine = None
 
+
 class _Engine(object):
-    def __init__(self,connect):
+    def __init__(self, connect):
         self._connect = connect
+
     def connect(self):
         return self._connect
 
-#实现懒连接数据库
-class _LasyConnection(object):
+# 实现懒连接数据库
+
+
+class _LazyConnection(object):
     def __init__(self):
-        self.connectoin=None
+        self.connection = None
 
     def cursor(self):
-        if self.connectoin is None:
-            self.connectoin=engine.connect()
+        if self.connection is None:
+            self.connection = engine.connect()
+            return self.connection.cursor()
+
     def commit(self):
-        self.connectoin.commit()
+        self.connection.commit()
 
     def rollback(self):
-        self.connectoin.rollback()
+        self.connection.rollback()
 
     def cleanup(self):
         if self.connection:
             self.connection.close()
-            self.connection=None
+            self.connection = None
 
-#持有数据库连接上下文对象
+# 持有数据库连接上下文对象
+
+
 class _DbCtx(threading.local):
     def __init__(self):
-        self.conection=None
-        self.transactions=0
+        self.connection = None
+        self.transactions = 0
 
     def is_init(self):
-        return not self.conection is None
+        return self.connection is not None
 
     def init(self):
-        self.conection=_LasyConnection()
+        self.connection = _LazyConnection()
         self.transactions = 0
 
     def cleanup(self):
@@ -54,19 +63,20 @@ class _DbCtx(threading.local):
         return self.connection.cursor()
 
 
-class _ConectionCtx(object):
+class _ConnectionCtx(object):
     def __enter__(self):
         global _db_ctx
-        self.should_cleanup=False
+        self.should_cleanup = False
         if not _db_ctx.is_init():
             _db_ctx.init()
-            self.should_cleanup=True
+            self.should_cleanup = True
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         global _db_ctx
         if self.should_cleanup:
             _db_ctx.cleanup()
+
 
 class _TransactionCtx(object):
     def __enter__(self):
@@ -82,7 +92,7 @@ class _TransactionCtx(object):
         global _db_ctx
         _db_ctx.transactions = _db_ctx.transactions - 1
         try:
-            if _db_ctx.transactions==0:
+            if _db_ctx.transactions == 0:
                 if exctype is None:
                     self.commit()
                 else:
@@ -91,72 +101,96 @@ class _TransactionCtx(object):
             if self.should_close_conn:
                 _db_ctx.cleanup()
 
-    def commit(self):
+    @staticmethod
+    def commit():
         global _db_ctx
         try:
             _db_ctx.connection.commit()
-        except:
+        except BaseException:
             _db_ctx.connection.rollback()
             raise
 
-    def rollback(self):
+    @staticmethod
+    def rollback():
         global _db_ctx
         _db_ctx.connection.rollback()
 
 
 def connection():
-    return _ConectionCtx()
+    return _ConnectionCtx()
+
 
 def transaction():
     return _TransactionCtx()
 
-'''
-with_connecton decorator
-'''
+
+"""
+with_connection decorator
+"""
+
+
 def with_connection(func):
     @functools.wraps(func)
-    def _wrapper(*args,**kw):
+    def _wrapper(*args, **kw):
         with connection():
             return func(*args, **kw)
     return _wrapper
 
-''''
+
+'''
 with_transaction decorator
 '''
+
+
 def with_transaction(func):
     @functools.wraps(func)
-    def _wrapper(*args,**kw):
+    def _wrapper(*args, **kw):
         with transaction():
-            return func(*args,**kw)
+            return func(*args, **kw)
     return _wrapper
 
-#数据库连接
+# 数据库连接
+
+
 class DBError(StandardError):
     pass
 
-#创建数据库引擎
-def create_engine(user,password,database):
+# 创建数据库引擎
+
+
+def create_engine(user, password, database):
     import mysql.connector
     global engine
-    if not engine is None:
+    if engine is not None:
         raise DBError('连接已初始化...')
-    conn=mysql.connector.connect(user, password, database, use_unicode=True)
-    engine=_Engine(conn)
+    conn = mysql.connector.connect(user=user, password=password, database=database, use_unicode=True)
+    engine = _Engine(conn)
 
-_db_ctx=_DbCtx()
+
+_db_ctx = _DbCtx()
 
 """
 查询语句
 """
+
+
 @with_connection
-def select(sql,*args):
+def select(sql, *args):
     global _db_ctx
-    cursor=None
-    sql=sql.replace('?','%s')
+    cursor = None
+    sql = sql.replace('?', '%s')
     try:
-        cursor=_db_ctx.connection.cursor()
-        cursor.execute(sql,args)
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
         return cursor.fetchall()
     finally:
         if cursor:
             cursor.close()
+
+
+if __name__ == '__main__':
+
+    create_engine('root', 'root', 'dbwl')
+    values = select('select *from sys_user')
+    for x in values:
+        print x
